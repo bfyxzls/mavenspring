18ceef21a2dd049aa7081423bb4de9e2e74508fd
zhangzhanling <zhangzhanling@chinalawinfo.com>
2019-11-13 15:10:07 +0800

    添加了工作相关

--- src/main/java/com/lind/mavenspring/MavenSpringApplication.java
+++ src/main/java/com/lind/mavenspring/MavenSpringApplication.java
@@ -1,12 +1,20 @@
 package com.lind.mavenspring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+    org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+    org.activiti.spring.boot.SecurityAutoConfiguration.class
+})
 public class MavenSpringApplication {
-  public static void main(String[] args){
-    SpringApplication.run(MavenSpringApplication.class,args);
-  }
 
+  public static void main(String[] args) {
+    SpringApplication.run(MavenSpringApplication.class, args);
+  }
 }
