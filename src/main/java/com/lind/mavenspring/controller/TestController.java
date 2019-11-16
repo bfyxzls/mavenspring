44a4eca7a9344503d152ba10495bb47790f18e0f
张占岭 <bfyxzls@outlook.com>
2019-11-16 20:25:24 +0800

    添加了异步处理方案，高并发时使用一个后台线程处理主程序的任务

--- src/main/java/com/lind/mavenspring/controller/TestController.java
+++ src/main/java/com/lind/mavenspring/controller/TestController.java
@@ -2,8 +2,10 @@ package com.lind.mavenspring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @RestController
 public class TestController {
@@ -11,4 +13,36 @@ public class TestController {
     public String hello(HttpServletRequest request) {
         return request.getParameter("lind");
     }
+
+    /**
+     * 异步建立高并发的订单.
+     *
+     * @return
+     */
+    @GetMapping("/create-order")
+    public DeferredResult<Object> createOrder() {
+        DeferredResult<Object> deferredResult = new DeferredResult<>((long) 3000, "error order");
+        OrderQueue.put(deferredResult);
+        return deferredResult;
+    }
+
+    /**
+     * 事实上它是一个订单队列的消费者，在后台写订单，并通知到create-order中.
+     *
+     * @return
+     */
+    @GetMapping("/process-order")
+    public String processOrder() {
+        //建立订单
+        String order = UUID.randomUUID().toString();
+        DeferredResult<Object> deferredResult = OrderQueue.pop();
+        if (deferredResult == null) {
+            return "没有要处理的任务";
+        }
+        //当设置之后，create-order将成功响应
+        deferredResult.setResult(order);
+        return order;
+    }
+
+
 }
