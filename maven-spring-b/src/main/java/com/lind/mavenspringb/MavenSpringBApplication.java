e2bb217832a9dfec1b311fb50ddef81898d8bf73
zhangzhanling <zhangzhanling@chinalawinfo.com>
2019-12-27 14:53:22 +0800

    åæ添加了es控制器

--- maven-spring-b/src/main/java/com/lind/mavenspringb/MavenSpringBApplication.java
+++ maven-spring-b/src/main/java/com/lind/mavenspringb/MavenSpringBApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class MavenSpringBApplication {
--- maven-spring-core/src/main/java/com/lind/mavenspringcore/MavenSpringCoreApplication.java
+++ maven-spring-core/src/main/java/com/lind/mavenspringcore/MavenSpringCoreApplication.java
@@ -3,10 +3,8 @@ package com.lind.mavenspringcore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {
